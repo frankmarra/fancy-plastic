fb3803e988c17be9b7b504d2be86b3487359d09a
Frank Marra <frankrmarra@gmail.com>
2024-04-12 01:03:20 -0400

    created coming soon page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Fancy Plastic'
+export const SITE_DESCRIPTION = 'Welcome to the home of Fancy Plastic!'
